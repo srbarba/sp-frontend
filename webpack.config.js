f5f23ef9622e2d7d6b37e54eceb96f01984ff578
Juan Pedro <jpbs92@hotmail.com>
2019-11-17 11:47:55 +0100

    Add bootstrap to the project

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ const CompressionPlugin = require('compression-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const postcssNormalize = require('postcss-normalize');
 
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
 
 module.exports = function (env, argv) {
   const isProduction = argv.mode === 'production';
@@ -68,45 +72,109 @@ module.exports = function (env, argv) {
         { test: /\.(jpg|png|gif|svg)$/, use: ['image-webpack-loader'], enforce: 'pre', exclude: /node_modules/ },
         { test: /\.svg$/, use: ['@svgr/webpack', 'url-loader'], exclude: /node_modules/ },
         { test: /\.(ts|js)x?$/, use: ['babel-loader'], exclude: /node_modules/ },
-        { test: /\.css$/, use: ['style-loader', 'css-loader'], exclude: /node_modules/ },
         { test: /\.js$/, use: ['source-map-loader'], enforce: 'pre', exclude: /node_modules/ },
         {
-          test: /\.s(a|c)ss$/,
-          exclude: /node_modules/,
-          loader: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          test: cssRegex,
+          exclude: cssModuleRegex,
+          use: getStyleLoaders(isProduction, { sourceMap: !isProduction }),
+          sideEffects: true,
+        },
+        {
+          test: cssModuleRegex,
+          use: getStyleLoaders(
+            isProduction,
             {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                sourceMap: !isProduction
-              }
-            },
+              importLoaders: 1,
+              sourceMap: !isProduction,
+              modules: true
+            }
+          ),
+        },
+        {
+          test: sassRegex,
+          exclude: sassModuleRegex,
+          use: getStyleLoaders(
+            isProduction,
             {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: !isProduction
-              }
+              sourceMap: !isProduction
             },
-            {
-              loader: 'postcss-loader', 
-              options: {
-                ident: 'postcss',
-                plugins: () => [
-                  postcssNormalize()
-                ]
-              }
-            },
+            'sass-loader',
             {
               loader: 'sass-resources-loader',
               options: {
                 resources: [path.resolve(__dirname, 'lib/sprinter/themes/default/_variables.scss')]
               }
+            }
+          ),
+          sideEffects: true,
+        },
+        {
+          test: sassModuleRegex,
+          use: getStyleLoaders(
+            isProduction,
+            {
+              sourceMap: !isProduction,
+              modules: true
             },
-          ]
+            'sass-loader',
+            {
+              loader: 'sass-resources-loader',
+              options: {
+                resources: [path.resolve(__dirname, 'lib/sprinter/themes/default/_variables.scss')]
+              }
+            }
+          )
         }
       ]
     },
     plugins: webpackPlugins
   }
+};
+
+function getStyleLoaders(isProduction, cssOptions, preProcessor, extraLoader) {
+  const loaders = [
+    isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+    {
+      loader: require.resolve('css-loader'),
+      options: cssOptions,
+    }
+  ].filter(Boolean);
+  if (preProcessor) {
+    loaders.push(
+      {
+        loader: require.resolve('resolve-url-loader'),
+        options: {
+          sourceMap: !isProduction,
+        },
+      },
+      {
+        loader: require.resolve(preProcessor),
+        options: {
+          sourceMap: true,
+        },
+      }
+    );
+  }
+
+  if (extraLoader) loaders.push(extraLoader);
+
+  // imported after preProcessor to allow @import "normalize.css";
+  loaders.push({
+    loader: require.resolve('postcss-loader'),
+    options: {
+      ident: 'postcss',
+      plugins: () => [
+        require('postcss-flexbugs-fixes'),
+        require('postcss-preset-env')({
+          autoprefixer: {
+            flexbox: 'no-2009',
+          },
+          stage: 3,
+        }),
+        postcssNormalize(),
+      ],
+      sourceMap: !isProduction,
+    },
+  });
+  return loaders;
 };
